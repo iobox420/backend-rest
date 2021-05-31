b2a32674d2ddeb09f8b15bcecf1e1410d7410f3e
alex-orlov <67315235+iobox420@users.noreply.github.com>
2021-05-31 18:27:17 +0500

    com-2-push

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3500
+const port = process.env.PORT || 80
 const bodyParser = require('body-parser')
 const passport = require('passport')
 /*const hostname = 'localhost';*/
