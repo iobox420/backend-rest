9ca6d59c4044e27e46b3faeeda263b5c32aff58a
alex-orlov <67315235+iobox420@users.noreply.github.com>
2021-06-01 15:29:20 +0500

    com-2-push

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express')
+/*const express = require('express')
 const app = express()
 const port = process.env.PORT || 80
 const bodyParser = require('body-parser')
 const passport = require('passport')
-/*const hostname = 'localhost';*/
+/!*const hostname = 'localhost';*!/
 const hostname = '185.251.89.161'
 
 app.use(bodyParser.urlencoded({extended:true}))
@@ -16,12 +16,11 @@ routes(app)
 
 app.listen(port, hostname,() => {
     console.log(`App listen on port ${port}`);
-})
-/*
+})*/
 
 const http = require('http')
 
-const hostname = 'localhost';
+const hostname = '185.251.89.161';
 const port = 80;
 
 const server = http.createServer((req, res) => {
@@ -33,4 +32,3 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
-*/
